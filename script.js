c417f6a246eb33a561f8db1dd5e78b4be20b1a10
razbehnam <161021689+razbehnam@users.noreply.github.com>
2026-08-01 17:32:53 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-function secret() {
+function openLetter(){
 
-    document.getElementById("message").innerHTML =
-    "فاطمه ❤️<br><br>ممنونم که وارد زندگیم شدی. امیدوارم همیشه کنار هم بخندیم، خوشحال باشیم و هر روز بیشتر از قبل عاشق هم بشیم. تولدت مبارک عشق زندگیم 🌹💖";
+    document.getElementById("letter").classList.remove("hidden");
 
 }
+
+
+
+const startDate = new Date("2023-05-01");
+
+
+function updateCounter(){
+
+    const now = new Date();
+
+
+    const difference = now - startDate;
+
+
+    const days = Math.floor(
+        difference / (1000 * 60 * 60 * 24)
+    );
+
+
+    const hours = Math.floor(
+        (difference / (1000 * 60 * 60)) % 24
+    );
+
+
+    const minutes = Math.floor(
+        (difference / (1000 * 60)) % 60
+    );
+
+
+    document.getElementById("counter").innerHTML =
+    days + " روز ❤️ " +
+    hours + " ساعت 💕 " +
+    minutes + " دقیقه";
+
+
+}
+
+
+setInterval(updateCounter,1000);
+
+
+updateCounter();
