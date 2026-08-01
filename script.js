2aeaeef6cf5adb9a7e33060d5b1ecf824efcd76a
razbehnam <161021689+razbehnam@users.noreply.github.com>
2026-08-02 01:58:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,34 @@
-function openLetter(){
+function openLetter() {
 
     document.getElementById("letter").classList.remove("hidden");
 
     const music = document.getElementById("bgMusic");
-    music.play();
+
+    music.play().catch(err => {
+        console.log(err);
+    });
 
 }
-
-
-
 
 const startDate = new Date("2023-05-01");
 
-
-function updateCounter(){
+function updateCounter() {
 
     const now = new Date();
 
-
     const difference = now - startDate;
 
-
-    const days = Math.floor(
-        difference / (1000 * 60 * 60 * 24)
-    );
-
-
-    const hours = Math.floor(
-        (difference / (1000 * 60 * 60)) % 24
-    );
-
-
-    const minutes = Math.floor(
-        (difference / (1000 * 60)) % 60
-    );
-
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((difference / (1000 * 60)) % 60);
 
     document.getElementById("counter").innerHTML =
-    days + " روز ❤️ " +
-    hours + " ساعت 💕 " +
-    minutes + " دقیقه";
-
+        days + " روز ❤️ " +
+        hours + " ساعت 💕 " +
+        minutes + " دقیقه";
 
 }
 
-
-setInterval(updateCounter,1000);
-
+setInterval(updateCounter, 1000);
 
 updateCounter();
